c8175acdd638341a4668edbab5185f4e1bd1d168
keaw <chatpimuk.klin@mail.ac.th>
2022-09-11 17:51:01 +0700

    CCCCCCC

--- minMedMax.js
+++ minMedMax.js
@@ -1,27 +1,27 @@
  const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  let k1;
-  let com;
-  let k2;
-  let array = [n1, n2, n3]
+
+  let array = [n1, n2, n3], swap, com = false,swaped ;
   while (!com) {
-    k2 =0;
-    for(i =1; i < 3; i++){
-      if(array[i -1] > array[i]){
-        k1 = array[i];
+    swaped = 0;
+    for(i =1; i < array.length; i++){
+      if(array[i -1] > array[i]) {
+        swap = array[i];
         array[i] = array[i -1];
-        array[i -1] = k1;
-        k2 =1;
+        array[i -1] = swap;
+        swaped =1;  
       };
-      if (k2 == 0){
+      if (swaped == 0){
         com = true;
       }
     };
   };
-    return {max: array[2],med: array[1],min: array[0]};
+    return {array};
 //dada
 };
+
+console.log(minMedMax(124, 32, 7));
 
 
 module.exports = minMedMax
